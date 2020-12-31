9b53785445e27272343fc4ba7f4befec62f5c3a8
David S <davidc.sharpe2@gmail.com>
2020-12-30 22:39:32 -0500

    Add config files

--- updscript.sh
+++ updscript.sh
@@ -3,5 +3,10 @@
 # Update package list
 pacman -Qqe > ./packages.list # All packages
 pacman -Qqm > ./aur.list      # AUR only
-
 # Backup cmd: pacman -S --needed
+
+# Config files
+mkdir ./homeconfig
+cp ~/.config/compton.conf ./homeconfig/  # Compton (compositor)
+cp ~/.config/autostart    ./homeconfig/  # Autostart
+cp ~/.config/i3           ./homeconfig/  # i3 (-gaps, wm)

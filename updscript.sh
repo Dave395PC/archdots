dc971ccb27ef368d0a120c430371828efcc77b32
David S <davidc.sharpe2@gmail.com>
2020-12-30 22:48:45 -0500

    Added more configs & ran script

--- updscript.sh
+++ updscript.sh
@@ -6,7 +6,9 @@ pacman -Qqm > ./aur.list      # AUR only
 # Backup cmd: pacman -S --needed
 
 # Config files
-mkdir ./homeconfig
+#mkdir ./homeconfig
 cp ~/.config/compton.conf ./homeconfig/  # Compton (compositor)
-cp ~/.config/autostart    ./homeconfig/  # Autostart
-cp ~/.config/i3           ./homeconfig/  # i3 (-gaps, wm)
+cp -r ~/.config/autostart ./homeconfig/  # Autostart
+cp -r ~/.config/i3        ./homeconfig/  # i3 (-gaps, wm)
+#mkdir ./etc
+cp /etc/fstab             ./etc/         # fstab
